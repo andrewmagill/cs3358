7809e26e11de66aad6a96ad4033ec6e7701cc81d
= <andrew.b.magill@gmail.com>
2015-02-06 00:31:16 -0600

    separating interface and implementation

--- book/ch2/IntCell.h
+++ book/ch2/IntCell.h
@@ -1,24 +1,23 @@
+#ifndef _IntCell_H_
+#define _IntCell_H_
+
 // Data Structures and Problem Solving Using C++ (2nd Edition) by Mark A. Weiss
 // A class for simulating an integer memory cell.
 
 Class IntCell
 {
   public:
-  
-      // Construct the IntCell.  Initial value is initialValue.
-      // Use explicit to avoid unwanted type conversion (since this 
-      // is a one parameter constructor).
+    // Construct the IntCell.  Initial value is initialValue.
+    // Use explicit to avoid unwanted type conversion (since this 
+    // is a one parameter constructor).
     explicit IntCell( int initialValue = 0 )
-      : storedValue( initialValue ) { }
-  
-      // Return the stored value.
-    int read( )
-      { return storedValue; }
-  
-      // Change the stored value to x.
-    void write( int x )
-      { storedValue = x; }
+    // Return the stored value.
+    int read( ) const;  
+    // Change the stored value to x.
+    void write( int x );
   
   private:
     int storedValue;
-}
+};
+
+#endif
