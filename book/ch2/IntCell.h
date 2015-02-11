8f99524f9f78956630edb84591de14b3d707963e
= <andrew.b.magill@gmail.com>
2015-02-10 20:57:25 -0600

    typo

--- book/ch2/IntCell.h
+++ book/ch2/IntCell.h
@@ -4,13 +4,13 @@
 // Data Structures and Problem Solving Using C++ (2nd Edition) by Mark A. Weiss
 // A class for simulating an integer memory cell.
 
-Class IntCell
+class IntCell
 {
   public:
     // Construct the IntCell.  Initial value is initialValue.
     // Use explicit to avoid unwanted type conversion (since this 
     // is a one parameter constructor).
-    explicit IntCell( int initialValue = 0 )
+    explicit IntCell( int initialValue = 0 );
     // Return the stored value.
     int read( ) const;  
     // Change the stored value to x.
