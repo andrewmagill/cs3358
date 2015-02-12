ef0bc87f3f71cd6d71a8ca7efc5a38489e06e39b
= <andrew.b.magill@gmail.com>
2015-02-12 00:25:22 -0600

    i'm so dumb

--- hw2/deck.cpp
+++ hw2/deck.cpp
@@ -9,56 +9,37 @@ using namespace std;
 Deck::Deck() {
   int index = 0;
   
-  // construct deck
-  for (int i = 0; i < SUIT_SIZE; i++) { 
-    for (int j = 1; j < RANK_SIZE; j++) { 
-      Card::Suit suit;
+  for (int intSuit = 0; intSuit < SUIT_SIZE; intSuit++) { 
+    for (int intRank = 0; intRank < RANK_SIZE; intRank++) { 
+      Card::Suit suit = (Card::Suit)intSuit;
   
-      switch(i) {
-         case 0: suit = Card::spades; break;
-         case 1: suit = Card::hearts; break;
-         case 2: suit = Card::diamonds; break;
-         case 3: suit = Card::clubs; break;
-      };
-  
-      index = i*10+j-1;
-      myCards[index] = Card(j+1, suit);
+      index = intSuit*13+intRank;
+      myCards[index] = Card(intRank+1, suit);
     }
   }
+}
+
+void Deck::shuffle() {
+  int randCardIndex;  
+  Card tempCard;
   
-  // not sure why, but it works better if i do this here
-  // as opposed to in the random number function
   srand(time(0));
-}
-
-void Deck::shuffle() {  
-  int index = 0;
-  int randCardIndex;
   
-  //Card tempCard;
-  
-  for (int i = 0; i < SUIT_SIZE; i++) { 
-    for (int j = 1; j <= RANK_SIZE; j++) {       
-      index = index = i*10+j-1;
-      randCardIndex = rand() % 52;
-      
-      if(randCardIndex != index) {
-        //tempCard = myCards[randCardIndex];
-        //myCards[i+j] = tempCard;
-        //swap(myCards[i+j], myCards[randCardIndex]);
-        //cout << myCards[50] << endl;
-      }      
-    }
+  for (int i = 0; i < SIZE; i++) {    
+    randCardIndex = rand() % 52;
+    
+    if(randCardIndex != i) {
+      tempCard = myCards[i];
+      myCards[i] = myCards[randCardIndex];
+      myCards[randCardIndex] = tempCard;
+    }    
   }
+  
+  cout << endl << endl;
 }
 
-void Deck::printDeck() {  
-  int index = 0;
-  
-  for (int i = 0; i < SUIT_SIZE; i++) { 
-    for (int j = 1; j <= RANK_SIZE; j++) { 
-      index = i*10+j-1;
-      cout << (index) << ": " << myCards[index] << endl;
-    }
+void Deck::printDeck() {
+  for (int i = 0; i < SIZE; i++) {
+    cout << i << ": " << myCards[i] << endl;
   }
 }
