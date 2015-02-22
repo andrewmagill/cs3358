0654c147efefda44df57f2b6a75128b30a8c0503
Andrew Magill <andrew.b.magill@gmail.com>
2015-02-21 20:39:12 -0600

    hw3 first commit

---
+++ hw3/arr_list_3358.cpp
@@ -0,0 +1,131 @@
+//list_3358.cpp
+//implementation of a very simple curssor-based List ADT
+//This is a simple array implementation
+
+#include "list_3358.h"
+#include <iostream>
+
+
+using namespace std;
+using namespace cs3358;     
+      
+     List_3358::List_3358 ( ) {
+         currentSize = 0;
+         cursor = 0;
+      }
+      
+      
+      List_3358::List_3358 ( const List_3358 & src ) {
+      
+         currentSize = src.currentSize;
+         cursor = src.cursor;
+
+         for (int i = 0 ; i < currentSize; i++)
+            values[i] = src.values[i];
+      }
+      
+      
+      List_3358::~List_3358 ( ) {
+ 
+      }
+
+      //************************************
+      //Mutator(s)
+      
+      void List_3358::insert (const value_type & item) {
+         
+         if (!isFull())
+         {
+            if (atEOL())
+            {
+               values[cursor] = item;
+               currentSize++;
+               cursor++;
+            }
+            else
+            {
+               for (int i = currentSize; i > cursor; i--)
+                  values[i] = values[i-1];
+               values[cursor] = item;
+               currentSize++;
+            }
+          }
+ 
+     }        
+            
+         
+  
+      void List_3358::remove() {
+    
+         if (!atEOL() && !isEmpty())
+         {
+            for (int i = cursor; i < currentSize - 1; i++)
+               values[i] = values[i+1];
+            
+            currentSize--;
+         }
+   
+      }
+
+
+      void List_3358::reset ( ) {
+      
+         cursor = 0;
+
+      }
+
+
+      
+      bool List_3358::advance ( ) {
+         
+         if (atEOL())
+         {
+            return false;
+         }
+         else
+         {
+            cursor++;
+         }
+
+         return (!atEOL());
+ 
+      }
+
+      
+
+      //*******************
+      // Accessor(s)
+      
+
+      bool List_3358::isEmpty ( ) const {
+         
+         return (currentSize == 0);
+         
+      }
+
+
+   
+      bool List_3358::atEOL ( ) const {
+      
+         return (cursor == currentSize);   
+      }
+      
+
+
+
+
+      bool List_3358::isFull ( ) const {
+         
+         return (currentSize == MAX_ITEMS);
+      }
+
+
+            
+      List_3358::value_type List_3358::getCurrent ( ) const {
+         if(!atEOL())
+            return values[cursor];         
+      }
+
+
+
+      
---
+++ hw3/list_test.cpp
@@ -0,0 +1,123 @@
+//list_test.cpp
+//CS3358
+//tests a very simple cursor based list ADT
+
+#include <iostream>
+#include "list_3358.h"
+
+using namespace std;
+using namespace cs3358;
+
+void showList(List_3358 myList);
+bool search (List_3358 & myList, List_3358::value_type target);
+
+int main () {
+    
+   List_3358 myList;
+
+   myList.insert(42);
+  
+   myList.insert(52);
+ 
+   myList.insert(12);
+  
+   myList.insert(-42);
+   
+   myList.insert(2);
+  
+   myList.reset();
+   myList.advance();
+   cout << myList.getCurrent()<< endl;
+ 
+   cout << "test inserts" << endl;  
+   showList(myList);
+   cout << myList.getCurrent()<< endl;
+   
+   myList.reset();   
+   myList.insert(88);
+   
+   cout << "test inserts" << endl;  
+   showList(myList);
+   
+   
+   List_3358 myList2 = myList; //copy constructor
+
+   cout << "mylist 2..." << endl;   
+   showList(myList2);
+
+   while (myList.advance());
+     
+   cout << "test getCurrent at EOL" << endl;
+   if (!myList.atEOL())
+      cout <<"shouldn't be here..." << myList.getCurrent() << "boo" << endl;
+   else
+      cout << "at EOL" << endl;
+   
+   cout << endl;
+   myList.reset();
+   if (myList.advance())
+      cout << myList.getCurrent() << endl;
+      
+   cout << " test search" << endl;
+   if (search(myList,12))
+     cout << myList.getCurrent() << endl;
+    
+   myList.remove();
+   
+   cout << " test remove after search" << endl;   
+   showList(myList);
+   
+   cout << "show other list, test copy constructor" << endl;
+   showList(myList2);
+   
+   cout << " test search 42" << endl;
+   if (search(myList,42))
+     cout << myList.getCurrent() << endl;
+    
+   myList.remove();
+   
+   cout << " test remove after search" << endl;   
+   showList(myList);
+   
+   myList.remove();
+   
+   cout << "show other list, test copy constructor" << endl;
+   showList(myList2);
+ 
+   
+}   
+
+
+//prints list.
+//note that the cursor is at the EOL when finished
+void showList(List_3358 myList) {
+
+   myList.reset();
+
+   while (!myList.atEOL())
+   {
+      cout << myList.getCurrent() << endl;
+      myList.advance();
+   }
+   
+}
+
+//if found, places the cursor at the target and returns true
+//otherwise returns false
+bool search (List_3358 & myList, List_3358::value_type target) {
+
+   bool found = false;
+
+   myList.reset();
+
+   while (!found && !myList.atEOL())
+   {
+      if (target == myList.getCurrent())
+         found = true;
+      else
+         myList.advance();
+   }
+
+   return found;
+    
+}
