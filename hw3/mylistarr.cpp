591627b6741f767dc07d108a04ef6bb309fda3fa
= <andrew.b.magill@gmail.com>
2015-02-25 00:49:54 -0600

    accessors

--- hw3/mylistarr.cpp
+++ hw3/mylistarr.cpp
@@ -1,5 +1,5 @@
-#include "list_3358_arrays.h"
 #include <iostream>
+#include "list_3358_arrays.h"
 
 using namespace std;
 using namespace cs3358;
@@ -19,6 +19,10 @@ List_3358::List_3358 ( const List_3358 & src ) {
     values[i] = src.values[i];
 }
 
+List_3358::~List_3358 ( ) {
+
+}
+
 // mutators
 
 void List_3358::insert ( const value_type & item ) {
@@ -26,31 +30,39 @@ void List_3358::insert ( const value_type & item ) {
 }
   
 void List_3358::remove ( ) {
-
+  cursor = 0;
 }
     
 void List_3358::reset ( ) {
 
 }
-      
-bool List_3358::advance ( ) {
 
+bool List_3358::advance ( ) {
+  
+  if (atEOL()) {
+    return false;
+  } else {
+    currentSize++;
+  }
+  
+  return (!atEOL());
 }
 
 // accessors
 
 bool List_3358::isEmpty ( ) const {
-
+  return (currentSize == 0);
 }
 
 bool List_3358::atEOL ( ) const {
-
+  return (cursor == currentSize);
 }
 
 bool List_3358::isFull ( ) const {
-
+  return (currentSize == MAX_ITEMS);
 }
 
 List_3358::value_type List_3358::getCurrent ( ) const {
-
+  if(!atEOL())
+    return values[cursor];
 }
--- hw3/mytest.cpp
+++ hw3/mytest.cpp
@@ -1,12 +1,24 @@
-#include "list_3358_arrays.h"
 #include <iostream>
+#include "list_3358_arrays.h"
 
 using namespace std;
 using namespace cs3358;
 
 int main() {
 
-  cout << "hello world" << endl;
+  List_3358 myList;
+  
+  myList.advance();
+  
+  cout << "is empty: " << myList.isEmpty() << endl;
+  
+  cout << "is at ed of list: " << myList.atEOL() << endl;
+  
+  cout << "get current (may be invalid): " << myList.getCurrent() << endl;
+  
+  myList.reset();
+  
+  cout << "is empty: " << myList.isEmpty() << endl;
   
   return 0;
 }
