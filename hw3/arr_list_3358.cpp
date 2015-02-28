8110893c50d2ce4181f89a37b928ce9d19ac1e4b
= <andrew.b.magill@gmail.com>
2015-02-28 05:10:05 -0600

    finished array list

--- hw3/arr_list_3358.cpp
+++ hw3/arr_list_3358.cpp
@@ -9,123 +9,122 @@
 using namespace std;
 using namespace cs3358;     
       
-     List_3358::List_3358 ( ) {
-         currentSize = 0;
-         cursor = 0;
+List_3358::List_3358 ( ) {
+   currentSize = 0;
+   cursor = 0;
+}      
+
+List_3358::List_3358 ( const List_3358 & src ) {
+
+   currentSize = src.currentSize;
+   cursor = src.cursor;
+
+   for (int i = 0 ; i < currentSize; i++)
+      values[i] = src.values[i];
+}
+
+
+List_3358::~List_3358 ( ) {
+
+}
+
+//************************************
+//Mutator(s)
+
+void List_3358::insert (const value_type & item) {
+
+   if (!isFull())
+   {
+      if (atEOL())
+      {
+         values[cursor] = item;
+         currentSize++;
+         cursor++;
       }
-      
-      
-      List_3358::List_3358 ( const List_3358 & src ) {
-      
-         currentSize = src.currentSize;
-         cursor = src.cursor;
-
-         for (int i = 0 ; i < currentSize; i++)
-            values[i] = src.values[i];
+      else
+      {
+         for (int i = currentSize; i > cursor; i--)
+            values[i] = values[i-1];
+         values[cursor] = item;
+         currentSize++;
       }
-      
-      
-      List_3358::~List_3358 ( ) {
- 
-      }
+    }
 
-      //************************************
-      //Mutator(s)
-      
-      void List_3358::insert (const value_type & item) {
-         
-         if (!isFull())
-         {
-            if (atEOL())
-            {
-               values[cursor] = item;
-               currentSize++;
-               cursor++;
-            }
-            else
-            {
-               for (int i = currentSize; i > cursor; i--)
-                  values[i] = values[i-1];
-               values[cursor] = item;
-               currentSize++;
-            }
-          }
- 
-     }        
-            
-         
-  
-      void List_3358::remove() {
-    
-         if (!atEOL() && !isEmpty())
-         {
-            for (int i = cursor; i < currentSize - 1; i++)
-               values[i] = values[i+1];
-            
-            currentSize--;
-         }
-   
-      }
+}        
 
 
-      void List_3358::reset ( ) {
-      
-         cursor = 0;
 
-      }
+void List_3358::remove() {
 
+   if (!atEOL() && !isEmpty())
+   {
+      for (int i = cursor; i < currentSize - 1; i++)
+         values[i] = values[i+1];
 
-      
-      bool List_3358::advance ( ) {
-         
-         if (atEOL())
-         {
-            return false;
-         }
-         else
-         {
-            cursor++;
-         }
+      currentSize--;
+   }
 
-         return (!atEOL());
- 
-      }
-
-      
-
-      //*******************
-      // Accessor(s)
-      
-
-      bool List_3358::isEmpty ( ) const {
-         
-         return (currentSize == 0);
-         
-      }
+}
 
 
-   
-      bool List_3358::atEOL ( ) const {
-      
-         return (cursor == currentSize);   
-      }
-      
+void List_3358::reset ( ) {
+
+   cursor = 0;
+
+}
+
+
+
+bool List_3358::advance ( ) {
+
+   if (atEOL())
+   {
+      return false;
+   }
+   else
+   {
+      cursor++;
+   }
+
+   return (!atEOL());
+
+}
+
+
+
+//*******************
+// Accessor(s)
+
+
+bool List_3358::isEmpty ( ) const {
+
+   return (currentSize == 0);
+
+}
+
+
+
+bool List_3358::atEOL ( ) const {
+
+   return (cursor == currentSize);   
+}
 
 
 
 
-      bool List_3358::isFull ( ) const {
-         
-         return (currentSize == MAX_ITEMS);
-      }
 
+bool List_3358::isFull ( ) const {
 
-            
-      List_3358::value_type List_3358::getCurrent ( ) const {
-         if(!atEOL())
-            return values[cursor];         
-      }
+   return (currentSize == MAX_ITEMS);
+}
 
 
 
-      
+List_3358::value_type List_3358::getCurrent ( ) const {
+   if(!atEOL())
+      return values[cursor];         
+}
+
+
+
+
--- hw3/mytest.cpp
+++ hw3/mytest.cpp
@@ -4,39 +4,149 @@
 using namespace std;
 using namespace cs3358;
 
+void showList(List_3358 myList);
+bool search (List_3358 & myList, List_3358::value_type target);
+
 int main() {
 
+  // test constructor
   List_3358 myList;
   
-  myList.advance();
+  // test insert
+  myList.insert(9);
+  cout << "inserting 9" << endl;
+  cout << "myList:" << endl;
+  showList(myList);  
   
-  cout << "is empty: " << myList.isEmpty() << endl;
+  // test remove
+  // this is removing from the end of the array 
+  // and the end of the list  
+  myList.remove();  
+  cout << "removing item that is at the end of the array and at the end of the list" << endl;
+  cout << "myList: (should be empty)" << endl;
+  showList(myList);
   
-  cout << "is at end of list: " << myList.atEOL() << endl;
+  // add a few items then remove one
+  myList.insert(36);
+  cout << "inserting 36" << endl;
+  myList.insert(13);
+  cout << "inserting 13" << endl;
+  myList.insert(24);
+  cout << "inserting 24" << endl;
+  cout << "myList:" << endl;  
+  showList(myList);
+    
+  //myList.showList();
   
-  cout << "get current (may be invalid): " << myList.getCurrent() << endl;
+  cout << "removing item that is at the end of the array and at the end of the list" << endl;
+  myList.remove();
+  cout << "myList:" << endl;
+  showList(myList);  
   
+  // test reset
+  cout << "resetting cursor" << endl;
   myList.reset();
   
-  cout << "is empty: " << myList.isEmpty() << endl;
+  // test inserting in the front of the list
+  cout << "inserting at the beginning of the list" << endl;
+  myList.insert(99);
+  cout << "myList:" << endl;
+  showList(myList);
   
-  myList.insert(5);
-  myList.insert(4);
-  myList.insert(3);
-  myList.insert(2);
-  myList.insert(1);
+  // test inserting in the middle of the list
+  cout << "inserting in the middle of the list of the list" << endl;
+  myList.insert(58);
+  showList(myList);
   
-  myList.reset();
+  cout << "myList.getCurrent(): " << myList.getCurrent() << endl;
   
-  cout << "get current (may be invalid): " << myList.getCurrent() << endl;
-
-  myList.advance();
-  myList.advance();
+  cout << "removing item that is not at the end of the array, and not the end of the list" << endl;
+  myList.remove();
+  showList(myList);
   
-  cout << "get current (may be invalid): " << myList.getCurrent() << endl;
+  //myList.showList();
   
+  // advancing to the end of the list
+  cout << "advancing to the end of the list" << endl << endl;
+  do 
+  {
+    cout << "myList.getCurrent() " << myList.getCurrent() << endl;
+    myList.advance();
+    cout << "myList.advance() " << endl;
+  } while (!myList.atEOL());
+  cout <<  endl;
+  
+  //myList.showList();
+  
+  cout << "myList.atEOL(): " << myList.atEOL() << endl;
+  
+  // test removing item that is at the end of this list, but not at the end of the array
+  cout << "removing item that is at the end of the list, but not at the end of the array" << endl;
   myList.remove();
   
-  cout << "get current (may be invalid): " << myList.getCurrent() << endl;
+  showList(myList);
+  
+  //myList.showList();
+  
+  // finally removing from the end of the array, not the end of the list
+  myList.reset();
+  
+  cout << "removing from the end of the array, but not the end of the list" << endl;
+  myList.remove();
+    
+  showList(myList);
+  
+  // test the copy constructor
+  List_3358 copyList = myList;
+  
+  copyList.insert(75);
+  
+  cout << "copyList: "<< endl;
+  showList(copyList);
+  
+  cout << "myList: "<< endl;
+  showList(myList);
+  
   return 0;
+}
+
+
+//prints list.
+//note that the cursor is at the EOL when finished
+void showList(List_3358 myList) {
+
+  if(myList.isEmpty()) {
+   cout << "empty list" << endl;
+   return;
+  }
+
+  myList.reset();
+
+  while (!myList.atEOL())
+  {
+    cout << myList.getCurrent() << endl;
+    myList.advance();
+  }
+
+  cout << endl;
+}
+
+//if found, places the cursor at the target and returns true
+//otherwise returns false
+bool search (List_3358 & myList, List_3358::value_type target) {
+
+  bool found = false;
+
+  myList.reset();
+
+  while (!found && !myList.atEOL())
+  {
+    if (target == myList.getCurrent())
+       found = true;
+    else
+       myList.advance();
+  }
+
+  return found;
+    
 }
