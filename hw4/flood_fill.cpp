126a9970f25d0156c7c6c4b8e13d30a2ea1abcca
= <andrew.b.magill@gmail.com>
2015-03-14 12:43:28 -0500

    cleaning, no new logic

--- hw4/flood_fill.cpp
+++ hw4/flood_fill.cpp
@@ -7,86 +7,95 @@
 
 using namespace std;
 
-static const int PICTURE_HEIGHT = 7;
-static const int PICTURE_WIDTH = 17;
-
-static const int BOARD_HEIGHT = 9;
-static const int BOARD_WIDTH = 19;
-
 static const int OFFSET_X = 1;
 static const int OFFSET_Y = 1;
 
-typedef struct coord {
+struct Coord {
    int x;
    int y;
-} Coord;
-/*
-void generate(char (&board)[BOARD_HEIGHT][BOARD_WIDTH]) {
-  for(int y = 0; y<BOARD_HEIGHT; y++) {
-    for(int x = 0; x<BOARD_WIDTH; x++) {
-      board[y][x] = 'N';
+
+   Coord(int x=0, int y=0)
+    : x(x), y(y) {
+
     }
+
+    Coord& operator=(const Coord& src) {
+      x = src.x;
+      y = src.y;
+      return *this;
+    }
+
+    Coord operator+(const Coord& src) const {
+      return Coord(src.x+x, src.y+y);
+    }
+
+    bool operator==(const Coord& src) const {
+      return (x == src.x && y == src.y);
+    }
+};
+
+enum Direction { NORTH, SOUTH, EAST, WEST };
+
+void travel(vector<string> & box,
+  vector<vector<bool> > &visited,
+  Stack_3358<Coord> pixelStack,
+  Direction direction, Coord start,
+  char target, char fill) {
+
+ Coord diff;
+
+  switch(direction) {
+    case NORTH :
+      cout << "NORTH" << endl;
+      diff = Coord(0,-1);
+      break;
+    case SOUTH :
+      cout << "SOUTH" << endl;
+      diff = Coord(0,+1);
+      break;
+    case EAST :
+      cout << "EAST" << endl;
+      diff = Coord(+1,0);
+      break;
+    case WEST :
+      cout << "WEST" << endl;
+      diff = Coord(-1,0);
+      break;
+  }
+
+  while(box[start.y][start.x] == target) {
+    if(!visited[start.y][start.x])
+      pixelStack.push(start);
+    visited[start.y][start.x] = true;
+    start = start + diff;
   }
 }
 
-void fill(char (&board)[BOARD_HEIGHT][BOARD_WIDTH]) {
-  for(int y = OFFSET_Y; y<=PICTURE_HEIGHT; y++) {
-    for(int x = OFFSET_X; x<=PICTURE_WIDTH; x++) {
-      board[y][x] = 'f';
-    }
+void showPicture(vector<string> & box) {
+  string line;
+
+  for(int i = OFFSET_Y; i<box.size()-OFFSET_Y;i++){
+    line = box[i];
+    for(int j = OFFSET_X; j < line.size()-OFFSET_X; j++)
+      cout << line[j];
+    cout << "\n";
   }
 }
 
-void flood(char (&board)[BOARD_HEIGHT][BOARD_WIDTH]) {
-  for(int y = 0; y<BOARD_HEIGHT; y++) {
-    for(int x = 0; x<BOARD_WIDTH; x++) {
+Coord getStartingPosition(int maxHeight, int maxWidth) {
+  Coord start;
 
-    }
-  }
-}
-
-void show(char (&board)[BOARD_HEIGHT][BOARD_WIDTH]) {
-  for(int y = 0; y<BOARD_HEIGHT; y++) {
-    for(int x = 0; x<BOARD_WIDTH; x++) {
-      cout << board[y][x];
-    }
-    cout << endl;
-  }
-}
-*/
-void fill(vector<string> & box, vector<vector<bool> > &visited, Stack_3358<Coord> pixelStack, Coord start, char original, char originalColor) {
-  Coord north = start;
-  Coord south = start;
-  Coord east = start;
-  Coord west = start;
-
-  while(box[north.y][north.x] == originalColor) {
-    if(visited[north.y][north.x])
-      pixelStack.push(north);
-    visited[north.y][north.x] = true;
-    north.y--;
+  while( !((start.y>=1) && (start.y<=maxHeight)) ) {
+    cout << "\nEnter a row (1 to "<< maxHeight <<"): ";
+    cin >> start.y;
   }
 
-  while(box[south.y][south.x] == originalColor) {
-    if(visited[south.y][south.x])
-      pixelStack.push(south);
-    visited[south.y][south.x] = true;
-    south.y++;
+  while( !((start.x>=1) && (start.x<=maxWidth)) ) {
+    cout << "Enter a column (1 to " << maxWidth <<"): ";
+    cin >> start.x;
   }
 
-  while(box[east.y][east.x] == originalColor) {
-    if(visited[east.y][east.x])
-      pixelStack.push(east);
-    visited[east.y][east.x] = true;
-    east.y--;
-  }
-
-  while(box[west.y][west.x] == originalColor) {
-    if(visited[west.y][west.x])
-      pixelStack.push(west);
-    visited[west.y][west.x] = true;
-    west.y--;
-  }
+  return start;
 }
 
 int main (int argc, char * argv[]) {
@@ -122,34 +131,19 @@ int main (int argc, char * argv[]) {
 
   cout << "\noriginal picture: \n\n";
 
-  for(int i = OFFSET_Y; i<box.size()-OFFSET_Y;i++){
-    line = box[i];
-    for(int j = OFFSET_X; j < line.size()-OFFSET_X; j++)
-      cout << line[j];
-    cout << "\n";
-  }
+  showPicture(box);
 
-  int row = -1;
-  int column = -1;
+  int pictureHeight = box.size()-2;
+  int pictureWidth = linesize;
+
+  Coord start = getStartingPosition(pictureHeight, pictureWidth);
+
   char color = '-';
-
-  while(!((row>=1)&&(row<box.size()-1))) {
-    cout << "\nEnter a row (1 to "<< box.size()-2 <<"): ";
-    cin >> row;
-  }
-
-  while(!((column>=1)&&(column<linesize))) {
-    cout << "Enter a column (1 to " << linesize-1 <<"): ";
-    cin >> column;
-  }
 
   while(!isalpha(color)) {
     cout << "Enter a color (English letter): ";
     cin >> color;
   }
-  Coord start;
-  start.x = column;
-  start.y = row;
 
   char originalColor = box[start.y][start.x];
 
@@ -158,31 +152,14 @@ int main (int argc, char * argv[]) {
     return EXIT_FAILURE;
   }
 
+
   Stack_3358<Coord> pixelStack;
 
   //fill(box, visited, pixelStack, start, originalColor, color);
 
-/*
-  for(int i = OFFSET_Y; i<box.size()-OFFSET_Y;i++){
-    line = box[i];
-    for(int j = OFFSET_X; j < line.size()-OFFSET_X; j++) {
-
-    }
-  }
-*/
   cout << "\nflood filled picture \n\n";
 
-  for(int i = OFFSET_Y; i<box.size()-OFFSET_Y;i++){
-    line = box[i];
-    for(int j = OFFSET_X; j < line.size()-OFFSET_X; j++)
-      cout << line[j];
-    cout << "\n";
-  }
-
-  //char board[BOARD_HEIGHT][BOARD_WIDTH];
-  //generate(board);
-  //fill(board);
-  //show(board);
+  showPicture(box);
 
   return EXIT_SUCCESS;
 }
