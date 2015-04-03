f09fe8bc37a0402a59c227f94a0bfeb8dfd4810a
Andrew Magill <andrew.b.magill@gmail.com>
2015-04-03 02:25:38 -0500

    isPrime works for much bigger numbers than i anticipated.

--- hw5/mathStuff.cpp
+++ hw5/mathStuff.cpp
@@ -6,24 +6,34 @@ cs1428 10/15/07 (updated 04/02/15)
 CS 3358 Section 152
 Spring 2015
 HW 5 Part 1
-Recursion - Math Stuff
+Inefficient Math with Recursion
 */
 
 #include <iostream>
 #include <fstream>
+#include <math.h>
 
 using namespace std;
 
 //**********  Function Prototypes  ******************
 
 //function isPrime
-//input parameter - positive integer
+//input parameters - required: positive integer,
+//                   optional: positive integer
 //returns true if the number is prime, otherwise false
 //
-
-bool isPrime (int number);
-
-
+// isPrime(n) will call isPrime(n, floor(sqrt(n)), which
+// will then call itself recursively, decrementing the
+// value of the divisor by 1 on each call until divisor == 1
+//
+// alternatively, you could create two definitions of isPrimt,
+// the first with one parameter, the second with two parameters:
+// isPrime(int number) and isPrime(int number, int divisor),
+// where isPrime(int number) is really a helper function that calls
+// isPrime(int number, int divisor) with divisor = floor(sqrt(number))
+// this would be a nice solution if you were creating methods within
+// a class, and wanted to hide the two parameter method from the user
+bool isPrime (int number, int divisor = 0);
 
 //function printPrimes
 //input parameter - positive integer
@@ -49,7 +59,6 @@ int findFibo (int n);
 
 void findFactors (int number);
 
-
 //******************  MAIN  *****************************
 
 int main () {
@@ -62,11 +71,12 @@ int main () {
     cin >> testNum;
     cout << endl;
 
-    if (isPrime(testNum ))
+    if (isPrime(testNum))
        cout << testNum << " is prime." << endl;
     else
        cout << testNum << " is not prime." << endl;
 
+/*
     //test for printing primes
     cout << "Enter n to print the prime numbers between 1 and n: ";
     cin >> testNum;
@@ -93,7 +103,7 @@ int main () {
     findFactors(testNum);
 
     system("PAUSE");
-
+*/
     return 0;
 }
 
@@ -105,13 +115,40 @@ int main () {
 //returns true if the number is prime, otherwise false
 //
 
-bool isPrime (int number) {
-   const int START_DIVISOR = 2; //HINT: You may want a helper function
+bool isPrime (int number, int divisor) {
+cout << " ------ count me -------- " << "(divisor = " << divisor << ")" << endl;
+    // a prime number is by definition greater than 1
+    if( number <= 1)
+        return false;
 
-   //YOUR CODE GOES HERE
+    double squareRoot = sqrt(number);
+    int closeToSquareRoot = floor(squareRoot);
 
+    // let's not divide by zero, this was probably the
+    // default parameter, let's start at the top
+    if( divisor == 0 )
+        return isPrime(number, closeToSquareRoot);
+
+    // if we get to this point, the number is prime
+    if( divisor <= 1 )
+        return true;
+
+    // don't need to check divisors greater than sqrt(number)
+    if( divisor > squareRoot )
+        return isPrime(number, closeToSquareRoot);
+
+    // check for divisibility
+    int remainder = (number % divisor);
+
+    if(remainder == 0)
+        return false;
+    else
+        return isPrime(number, (divisor-1));
+
+    // if we get here we've done something wrong, we probably
+    // should throw an error, but let's return false
+    return false;
 }
-
 
 //function printPrimes
 //input parameter - positive integer
